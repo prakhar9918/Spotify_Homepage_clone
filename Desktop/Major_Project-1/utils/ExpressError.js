b674967eebca710fb74edb2a8146e6bceb5e3f00
prakhar9918 <136910948+prakhar9918@users.noreply.github.com>
2024-06-06 22:41:45 +0530

    Delete Desktop/Major_Project-1 directory

--- Desktop/Major_Project-1/utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error{
-    constructor(statusCode,messege){
-        super();
-        this.statusCode = statusCode;
-        this.message = messege;
-    }
-}
-
-module.exports = ExpressError;
