304e51f78b8a86d7856541d395dabd6e03291b9d
prakhar9918 <136910948+prakhar9918@users.noreply.github.com>
2024-06-15 17:31:55 +0530

    Delete Desktop/finance_webapp directory

--- Desktop/finance_webapp/route/user_route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const {userSignup}  = require('../controllers/user_controllers');
-const router = express.Router;
-
-router.post("/registeruser",userSignup);
-
-module.exports = router;
